7d84efea7f4a05e03dabe34fec7aabfff69db18d
rebekahheacock <rebekah.heacock@gmail.com>
2016-11-01 15:02:18 -0400

    Add actions to list and item components
    Add action to listr-list component that shows and hides list items
    Add action to listr-list/item component that toggles strike-through property

--- app/components/listr-list/item/component.js
+++ app/components/listr-list/item/component.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'li'
+  tagName: 'li',
+  classNameBindings: ['listItemStrikeThrough'],
+  listItemStrikeThrough: false,
+  actions: {
+    toggleProperty() {
+      return this.toggleProperty('listItemStrikeThrough');
+    }
+  },
 });
