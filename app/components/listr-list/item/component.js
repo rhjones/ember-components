c454fef5f74acb9b4dc20698316be4a9a138d737
rebekahheacock <rebekah.heacock@gmail.com>
2016-11-01 15:35:03 -0400

    Rename variables related to list item strikthrough toggling

--- app/components/listr-list/item/component.js
+++ app/components/listr-list/item/component.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'li',
-  classNameBindings: ['listItemStrikeThrough'],
-  listItemStrikeThrough: false,
+  classNameBindings: ['listItemCompleted'],
+  listItemCompleted: false,
   actions: {
-    toggleProperty() {
-      return this.toggleProperty('listItemStrikeThrough');
+    toggleStrikeThrough() {
+      return this.toggleProperty('listItemCompleted');
     }
   },
 });
